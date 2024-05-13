1ef3aec04a3ad3e2dee286c449829294cca5911f
jrobles0717 <j_robles0717@yahoo.com>
2024-05-12 20:26:56 -0400

    Added dummy data for featured posts page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
+import FeaturedPosts from "../components/home-page/featured-posts";
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 
 function HomePage() {
+  const DUMMY_POSTS = [
+    {
+      slug: "getting-started-with-nextjs",
+      title: "Getting Started With NestJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is the React framework for production - it makes building fullstack React app",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs2",
+      title: "Getting Started With NestJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is the React framework for production - it makes building fullstack React app",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs3",
+      title: "Getting Started With NestJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is the React framework for production - it makes building fullstack React app",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs4",
+      title: "Getting Started With NestJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is the React framework for production - it makes building fullstack React app",
+      date: "2022-02-10",
+    },
+  ];
+
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
