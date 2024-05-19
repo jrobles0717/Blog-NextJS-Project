3b9b9616d20f5ec92d34e78d1b1cce5dfffd179d
jrobles0717 <j_robles0717@yahoo.com>
2024-05-19 16:50:13 -0400

    Added dynamic data using files stored in the app

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,25 @@
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-function HomePage() {
-  const DUMMY_POSTS = [
-    {
-      slug: "getting-started-with-nextjs",
-      title: "Getting Started With NestJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for production - it makes building fullstack React app",
-      date: "2022-02-10",
-    },
-    {
-      slug: "getting-started-with-nextjs2",
-      title: "Getting Started With NestJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for production - it makes building fullstack React app",
-      date: "2022-02-10",
-    },
-    {
-      slug: "getting-started-with-nextjs3",
-      title: "Getting Started With NestJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for production - it makes building fullstack React app",
-      date: "2022-02-10",
-    },
-    {
-      slug: "getting-started-with-nextjs4",
-      title: "Getting Started With NestJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for production - it makes building fullstack React app",
-      date: "2022-02-10",
-    },
-  ];
-
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
