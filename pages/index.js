3c9c520bbfef2b63766b5032aba3dea5b179cd66
jrobles0717 <j_robles0717@yahoo.com>
2024-05-25 21:52:17 -0400

    Added Contact Form content, notifications and meta data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { Fragment } from "react";
+import Head from "next/head";
 import Hero from "../components/home-page/hero";
 import { getFeaturedPosts } from "../lib/posts-util";
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>John' Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
